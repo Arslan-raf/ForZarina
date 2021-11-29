4a7d2d4928ceb16c5940a0324b6169ebbcf2f354
Arslan-raf <61138541+Arslan-raf@users.noreply.github.com>
2021-11-29 23:01:53 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace программка_по_ИБ
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Start_button = new System.Windows.Forms.Button();
+            this.task_label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.solution_label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.algorithm_label1 = new System.Windows.Forms.Label();
+            this.algorithm_label2 = new System.Windows.Forms.Label();
+            this.go_button = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.minute_label = new System.Windows.Forms.Label();
+            this.second_label = new System.Windows.Forms.Label();
+            this.kek = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.result_button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Start_button
+            // 
+            resources.ApplyResources(this.Start_button, "Start_button");
+            this.Start_button.Name = "Start_button";
+            this.Start_button.UseVisualStyleBackColor = true;
+            this.Start_button.Click += new System.EventHandler(this.Start_button_Click);
+            // 
+            // task_label
+            // 
+            this.task_label.ForeColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.task_label, "task_label");
+            this.task_label.Name = "task_label";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // solution_label
+            // 
+            resources.ApplyResources(this.solution_label, "solution_label");
+            this.solution_label.Name = "solution_label";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // stop_button
+            // 
+            resources.ApplyResources(this.stop_button, "stop_button");
+            this.stop_button.Name = "stop_button";
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // algorithm_label1
+            // 
+            resources.ApplyResources(this.algorithm_label1, "algorithm_label1");
+            this.algorithm_label1.Name = "algorithm_label1";
+            // 
+            // algorithm_label2
+            // 
+            this.algorithm_label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.algorithm_label2.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.algorithm_label2, "algorithm_label2");
+            this.algorithm_label2.Name = "algorithm_label2";
+            // 
+            // go_button
+            // 
+            resources.ApplyResources(this.go_button, "go_button");
+            this.go_button.Name = "go_button";
+            this.go_button.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            resources.ApplyResources(this.dateTimePicker1, "dateTimePicker1");
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            // 
+            // minute_label
+            // 
+            this.minute_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.minute_label, "minute_label");
+            this.minute_label.Name = "minute_label";
+            // 
+            // second_label
+            // 
+            this.second_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.second_label, "second_label");
+            this.second_label.Name = "second_label";
+            this.second_label.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // kek
+            // 
+            this.kek.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.kek, "kek");
+            this.kek.Name = "kek";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // result_button
+            // 
+            resources.ApplyResources(this.result_button, "result_button");
+            this.result_button.Name = "result_button";
+            this.result_button.Click += new System.EventHandler(this.result_button_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(this.radioButton2, "radioButton2");
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            resources.ApplyResources(this.radioButton3, "radioButton3");
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            resources.ApplyResources(this.radioButton4, "radioButton4");
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            resources.ApplyResources(this.checkBox3, "checkBox3");
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // checkBox4
+            // 
+            resources.ApplyResources(this.checkBox4, "checkBox4");
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            resources.ApplyResources(this.checkBox5, "checkBox5");
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            resources.ApplyResources(this.checkBox6, "checkBox6");
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.result_button);
+            this.Controls.Add(this.kek);
+            this.Controls.Add(this.second_label);
+            this.Controls.Add(this.minute_label);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.go_button);
+            this.Controls.Add(this.algorithm_label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.solution_label);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.task_label);
+            this.Controls.Add(this.Start_button);
+            this.Controls.Add(this.algorithm_label2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_button;
+        private System.Windows.Forms.Label task_label;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label solution_label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label algorithm_label1;
+        private System.Windows.Forms.Label algorithm_label2;
+        private System.Windows.Forms.Button go_button;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label minute_label;
+        private System.Windows.Forms.Label second_label;
+        private System.Windows.Forms.Label kek;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button result_button;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace программка_по_ИБ
+{
+    public partial class Form1 : Form
+    {
+
+        int minute, second;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 500;
+            minute = 0;
+            second = 0;
+            second_label.Text = "00";
+            minute_label.Text = "00";
+            
+
+
+        }
+
+        private void Start_button_Click(object sender, EventArgs e)
+        {
+
+            task_label.Text = "Произошел киберинцидент! Срочно решите проблему!";
+
+            minute = 0;
+            second = 0;
+            minute_label.Text = "00";
+            second_label.Text = "00";
+            
+            timer1.Start();
+
+            if (timer1.Enabled)
+            {
+
+                go_button.Enabled = true;
+                stop_button.Enabled = true;
+
+            }
+            Start_button.Enabled = false;
+
+        }
+
+
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+
+            label1.Enabled = true;
+            label3.Enabled = true;
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            radioButton4.Enabled = true;
+            label4.Enabled = true;
+            checkBox1.Enabled = true;
+            checkBox2.Enabled = true;
+            checkBox3.Enabled = true;
+            checkBox4.Enabled = true;
+            checkBox5.Enabled = true;
+            checkBox6.Enabled = true;
+
+            result_button.Enabled = true;
+            timer1.Stop();
+            
+
+        }
+
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_button_Click(object sender, EventArgs e)
+        {
+            label2.Enabled = true;
+            textBox2.Enabled = true;
+            solution_label.Enabled = true;
+            textBox1.Enabled = true;
+            Start_button.Enabled = true;
+
+            if (radioButton1.Checked)
+            {
+                textBox2.Text = "Статья 272. Неправомерный доступ к компьютерной информации";
+                textBox1.Text = "Штраф в размере до двухсот тысяч рублей или в размере заработной платы или иного дохода осужденного за период до восемнадцати месяцев, либо исправительные работы на срок до одного года, либо ограничение свободы на срок до двух лет, либо принудительные работы на срок до двух лет, либо лишение свободы на тот же срок";
+            }
+
+            if (radioButton2.Checked)
+            {
+                textBox2.Text = "Статья 273. Создание, использование и распространение вредоносных компьютерных программ";
+                textBox1.Text = "Ограничение свободы на срок до четырех лет, либо принудительные работы на срок до четырех лет, либо лишение свободы на тот же срок со штрафом в размере до двухсот тысяч рублей или в размере заработной платы или иного дохода осужденного за период до восемнадцати месяцев";
+            }
+
+
+
+
+        }
+
+
+
+
+        //обработчик таймера (timer1)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (minute<3)
+            {
+                if (second < 59)
+                {
+                    second++;
+                    if (second < 10) { second_label.Text = "0" + second.ToString(); }
+                    else { second_label.Text = second.ToString(); }
+                }
+
+                else
+                {
+
+                        minute++;
+                        minute_label.Text = "0" + minute.ToString();
+                    second = 0;
+                    second_label.Text = "00" ;
+
+                    if (minute == 1)
+                        {
+
+                            algorithm_label2.Text = "Вы не успели решить пролему за минуту. Угроза переходит на 2 уровень \n";
+                        algorithm_label2.Text += "1)сделать что-то что-то \n";
+                        algorithm_label2.Text += "2)сделать то \n";
+                        algorithm_label2.Text += "3)сделать сё \n";
+
+                    }
+
+                        if (minute == 2)
+                        {
+
+                            algorithm_label2.Text = "Вы не успели решить пролему за 2 минуту. Угроза переходит на 3 уровень \n";
+                        algorithm_label2.Text += "1) попробуй  \n";
+                        algorithm_label2.Text += "2) давай  \n";
+                        algorithm_label2.Text += "3) ну же \n";
+
+
+                    }
+                }
+
+
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("время закончилось");
+                
+            }
+        }
+
+
+    }
+}
